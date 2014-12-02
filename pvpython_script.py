af0373c7e48a352c35218e1b423101d5eee3474b
Ali SAAD <alisaad05@gmail.com>
2014-12-02 20:01:49 +0100

    pvpython: Fixed many bugs in animation creation, directory listing and other minor tweaks

--- pvpython_script.py
+++ pvpython_script.py
@@ -1,21 +1,57 @@
-""" 
-Ali SAAD 2014
-Paraview offscreen script that exports PNG images from simulation results
-For best rendering results use with pvpython.exe version 4.2 (mainly for the .tex rendering)
+#TODO: replace directory strings by r strings so that "\" works instead of "\\"
+#TODO: Write wrapper class to hide the VTU processing, custom colors and color presets (in a similar way as vtktools)
+docstring= """ 
+************************* Ali SAAD 2014 ***************************
+
+* VTU/PVSM (paraview state) to PNG Utility 
+  + Crop 	..................(needs ImageMagick.exe) 
+  + Animation Creator ........(needs mencoder.exe)
+
+* Paraview offscreen script that exports PNG images from simulation
+  results in a seamless and fast way (faster than using GUI).
+
+* Supports selective data loading, i.e. user can specify the fields
+  to process instead of loading all data from the VTU file
+
+* Use with pvpython.exe version 4.2 located in the "bin" folder 
+  of Paraview installation (mainly for the .tex rendering and com-
+  patibility of some functions e.g. SaveScreenshot)
+
+*******************************************************************
 """
-# PATHS
-# =====
+print docstring
+
+
+# USER DATA (paths, options ...)
+# ==============================
+
+dirname =  "U:\\Intel_Cluster_Cases\\5_SMACS_LS\\"
 foldername = "A_retrait\\"
 filename = "out_thermique_"
-dirname =  "C:\\Users\\RASPI\\Google Drive\\WORK Shared Folder\\Python Paraview Offscreen\\Open result and animate\\"
 
-#CUSTOM OPTIONS 
-# ============
-SHOW_TIME = 	False	# True # False
+# Time options to determine the correct VTU file names
+t_start=0
+t_end=1001
+frequency_VTU=10
+P0_fields_list = ['Heaviside_M_p0']
+P1_fields_list = ['d_interface', 'd_interface_filtered','Vitesse']
+
+SHOW_CENTER_AXIS = False
+SHOW_ORIENTATION_AXIS = True
+
+# OUTPUT OPTIONS (DISABLE ALL IF RUNNING IN PARAVIEW'S PYTHON SHELL)
+# ====================================================================
+SHOW_TIME = 	True	# True # False
 EXPORT_PNG = 	True	# True # False
 CROP_PNG = 		True	# True # False
-MAKE_ANIMATION=	False	# True # False
-fps=5
+MAKE_ANIMATION=	True	# True # False
+OFFSCREEN_RENDERING = True
+fps=10
+LOW = [800,600]
+HIGH = [1280,1024]
+RESOLUTION = HIGH # LOW #  HIGH
+
+# ==========================================================================================================================
 
 #CUSTOM COLORS
 # ============
@@ -23,6 +59,18 @@ RED = [1,0,0]
 BLUE = [0,0,1]
 BLACK = [0,0,0]
 
+# PRESETS (painfully defined by hand :))
+# ========
+BlueToRedRainbow = [0.0, 0,0,1, 
+                    1.0, 1,0,0]
+RainbowDesaturated = [  0.0	 ,	0.278431, 	0.278431, 	0.858824, 
+						0.143, 	0.0,		0.0, 		0.360784, 
+						0.285, 	0.0,		1.0, 		1.0, 
+						0.429, 	0.0,		0.501961, 	0.0, 
+						0.571, 	1.0,		1.0, 		0.0, 
+						0.714, 	1.0,		0.380392, 	0.0, 
+						0.857, 	0.419608,	0.0, 		0.0, 
+						1.0, 	0.878431,	0.301961, 	0.301961 ]
 """
 Update views, isometric and other routines inpired by MAYAVI's tvtk_scene.py
 """
@@ -32,13 +80,13 @@ def update_view(VIEW, position, viewup, focalpoint = [0,0,0]): #x,y,z, vx,vy,vz)
 	VIEW.CameraFocalPoint = focalpoint
 
 def Z_plus_view(VIEW):
-	position = [0,0,1]
-	viewup = [0,1,0]
+	position = 	[0,0,1]
+	viewup = 	[0,1,0]
 	update_view(VIEW, position, viewup)
 	
 def isometric_view(VIEW):
-	position = [1,1,1]
-	viewup = [0,0,1]
+	position = 	[1,1,1]
+	viewup = 	[0,0,1]
 	update_view(VIEW, position, viewup)
 	
 def custom_view(VIEW):
@@ -51,133 +99,160 @@ def custom_view(VIEW):
 	update_view(VIEW, position, viewup, focalpoint)
 	
 	
-from paraview.simple import *
 import subprocess
 import os
 
-# START FROM STATE FILE
-# =======================
-""" The drawback with state files is that they depend on a result path that cannot be changed easily from the outside"""
-# statefile = dirname + foldername + "pv.pvsm"
-# servermanager.LoadState(statefile) #SaveState
-# reader  = FindSource("out_thermique_0*")
+if EXPORT_PNG: 
+	from paraview.simple import *
 
-# START FROM VTU FILES
-# =====================
-print("\n\nREADING VTU...")
-dir = dirname + foldername + "resultatsVTU\\" + filename
-reader = XMLUnstructuredGridReader( FileName= [dir+str(i).rjust(5,'0')+'.vtu' for i in range(0,40,10)]) #I fucking love python generators and list comprehension !!!
-# P1 FIELDS
-reader.PointArrayStatus = ['d_interface', 'd_interface_filtered','Vitesse']
-# P0 FIELDS
-reader.CellArrayStatus = ['Heaviside_M_p0']
-# Activate the "eye" icon next to the source
-Show() 
-# Read the time array
-timesteps = reader.TimestepValues
-# Misc
-data_representation = GetDisplayProperties(reader)
+	# START FROM STATE FILE
+	# =======================
+	""" The drawback with state files is that they depend on a result path that cannot be changed easily from the outside"""
+	# statefile = dirname + foldername + "pv.pvsm"
+	# servermanager.LoadState(statefile) #SaveState
+	# reader  = FindSource("out_thermique_0*")
 
-# CONTROL CAMERA
-# ================
-view = GetRenderView()
-view.ResetCamera()
-SetActiveView(view)
-view.ViewSize=[1280,1024] #[1280,1024]
-view.Background = [1,1,1]  #white
-view.CenterAxesVisibility = 0
-view.OrientationAxesVisibility = 0
-view.OrientationAxesLabelColor = BLUE
-#Offscreen rendering to be specified before calling the first Render()
-view.UseOffscreenRendering=1
-view.UseOffscreenRenderingForScreenshots=1
-custom_view(view) # Z_plus_view # custom_view
-Render()
+	# START FROM VTU FILES
+	# =====================
+	print("\nREADING VTU...")
+	dir = dirname + foldername + "resultatsVTU\\" + filename
+	reader = XMLUnstructuredGridReader( FileName= [dir+str(i).rjust(5,'0')+'.vtu' for i in range(t_start,t_end,frequency_VTU)]) #I f***ing love python generators and list comprehension !!!
+	# P1 FIELDS
+	reader.PointArrayStatus = P1_fields_list
+	# P0 FIELDS
+	reader.CellArrayStatus = P0_fields_list
+	# Activate the "eye" icon next to the source
+	Show() 
+	# Read the time array
+	timesteps = reader.TimestepValues
+	# Misc
+	data_representation = GetDisplayProperties(reader)
 
-# SHOW TIME ANNOTATION
-# ======================
-if SHOW_TIME is True:
-	annTime = AnnotateTimeFilter(reader)
-	annTimeDisplay = GetDisplayProperties(annTime, view=view)
-	annTimeDisplay.Color= RED
-	annTimeDisplay.Bold=1
-	Show(annTime)
+	# CONTROL CAMERA
+	# ================
+	view = GetRenderView()
+	view.ResetCamera()
+	SetActiveView(view)
+	view.ViewSize= RESOLUTION
+	view.Background = [1,1,1]  #white
+	view.CenterAxesVisibility = int(SHOW_CENTER_AXIS)
+	view.OrientationAxesVisibility = int(SHOW_ORIENTATION_AXIS)
+	view.OrientationAxesLabelColor = BLUE
+	#Offscreen rendering to be specified before calling the first Render()
+	view.UseOffscreenRendering= int(OFFSCREEN_RENDERING)
+	view.UseOffscreenRenderingForScreenshots= int(OFFSCREEN_RENDERING)
+	custom_view(view) # Z_plus_view # custom_view
+	Render()
 
-# DISPLAY VELOCITY FIELD (ONLY FOR VTU, DO NOT USE FOR STATE FILES)
-# =================================================================
-# SHOW SCALAR OR VECTOR
-min = 0.0
-max = 1.6e-10
-Vitesse_LUT = GetLookupTableForArray( "Vitesse", 3, 
-										Discretize=0, 
-										# RGBPoints=[min, 0.0, 0.0, 1.0, max, 1.0, 0.0, 0.0], 
-										VectorMode='Magnitude', 
-										# NanColor=[0.498039215686275, 0.498039215686275, 0.498039215686275], 
-										 
-										ScalarRangeInitialized=1.0, 
-										AllowDuplicateScalars=1 
-									)
+	# SHOW TIME ANNOTATION
+	# ======================
+	if SHOW_TIME :
+		annTime = AnnotateTimeFilter(reader)
+		annTime.Format = 'Time: %1.0f sec'
+		annTimeDisplay = GetDisplayProperties(annTime, view=view)
+		annTimeDisplay.FontFamily= 'Arial' # Arial # Times # Courier
+		annTimeDisplay.FontSize= 10
+		annTimeDisplay.Color= BLACK # BLUE
+		annTimeDisplay.Opacity= 1.0
+		annTimeDisplay.Bold=0
+		annTimeDisplay.Italic=0
+		annTimeDisplay.Shadow=0
+		
+		# annTimeDisplay.WindowLocation= "LowerLeftCorner"  # Upper/Lower + Left/Right + Corner , e.g. "LowerRightCorner" # or LowerCenter/UpperCenter # or "AnyLocation"
+		annTimeDisplay.Position = [0.04, 0.22]
+		Show(annTime)
 
-data_representation.ColorArrayName = 'Vitesse'
-data_representation.LookupTable = Vitesse_LUT
+	# DISPLAY VELOCITY FIELD (ONLY FOR VTU, DO NOT USE FOR STATE FILES)
+	# =================================================================
+	# SHOW SCALAR OR VECTOR
+	min = 1e-9
+	max = 5e-5
+	Vitesse_LUT = GetLookupTableForArray( "Vitesse", 3, 
+											Discretize=0, 
+											RGBPoints= BlueToRedRainbow ,   # BlueToRedRainbow # RainbowDesaturated
+											VectorMode='Magnitude', 
+											# NanColor=[0.498039215686275, 0.498039215686275, 0.498039215686275], 
+											ColorSpace='HSV', # RGB # HSV # Lab # Diverging
+											ScalarRangeInitialized=1.0, 
+											AllowDuplicateScalars=1 
+										)
 
-Vitesse_LUT.RescaleTransferFunction(min,max)
-Vitesse_LUT.LockScalarRange = 0
-Vitesse_LUT.Discretize = 0
-Vitesse_LUT.NumberOfTableValues = 20
-Vitesse_LUT.ColorSpace='HSV' # RGB # HSV # Lab # Diverging
+	data_representation.ColorArrayName = 'Vitesse'
+	data_representation.LookupTable = Vitesse_LUT
 
-Render()
+	Vitesse_LUT.RescaleTransferFunction(min,max)
+	Vitesse_LUT.LockScalarRange = 1
+	
+	Vitesse_LUT.Discretize = 1
+	Vitesse_LUT.NumberOfTableValues = 30
+	# Vitesse_LUT.ColorSpace='RGB' # RGB # HSV # Lab # Diverging
+	Render()
 
-# SHOW CORRESPONDING SCALAR BAR
-ScalarBar = CreateScalarBar()
-# ScalarBar = CreateScalarBar()
-ScalarBar = GetScalarBar(Vitesse_LUT, view)
-ScalarBar.Position = [0.75, 0.3] # this is position
-ScalarBar.Position2 = [0.2, 0.35] # this is size
-ScalarBar.Orientation = 'Vertical' # Horizontal # Vertical
-ScalarBar.Title = '$\\langle v^F \\rangle$'
-ScalarBar.TitleBold = 0
-ScalarBar.TitleItalic = 0
-ScalarBar.TitleJustification = 'Centered' # Centered # Left s# Right
-ScalarBar.ComponentTitle = '[m/s]'
-ScalarBar.TitleColor = BLUE
-ScalarBar.TitleFontSize = 15
-ScalarBar.DrawTickMarks = 0
-ScalarBar.AddRangeAnnotations = 1
-ScalarBar.AspectRatio = 15 #25
-# ScalarBar.TextPosition = 'Ticks left/bottom, annotations right/top'
-ScalarBar.TextPosition = 'Ticks right/top, annotations left/bottom'
+	# SHOW CORRESPONDING SCALAR BAR
+	ScalarBar = CreateScalarBar()
+	ScalarBar = GetScalarBar(Vitesse_LUT, view)
+	ScalarBar.Position = [0.75, 0.3] # this is position
+	ScalarBar.Position2 = [0.2, 0.35] # this is size
+	ScalarBar.Orientation = 'Vertical' # Horizontal # Vertical
+	ScalarBar.Title = '$\\langle v^F \\rangle$'
+	ScalarBar.TitleBold = 0
+	ScalarBar.TitleItalic = 0
+	ScalarBar.TitleOpacity = 1.0
+	ScalarBar.TitleFontFamily = 'Arial'
+	ScalarBar.TitleShadow = 0
+	ScalarBar.TitleJustification = 'Centered' # Centered # Left s# Right
+	ScalarBar.ComponentTitle = '[m/s]'
+	ScalarBar.TitleColor = BLUE
+	ScalarBar.TitleFontSize = 15
+	ScalarBar.AspectRatio = 15 #25
+	# ScalarBar.TextPosition = 'Ticks left/bottom, annotations right/top'
+	ScalarBar.TextPosition = 'Ticks right/top, annotations left/bottom'
 
-ScalarBar.LabelBold = 0
-ScalarBar.LabelItalic = 0
-ScalarBar.LabelColor = RED
-ScalarBar.LabelFontSize = 10
-ScalarBar.AutomaticLabelFormat = 0
-ScalarBar.DrawTickLabels = 0
-ScalarBar.LabelFormat = '%-#2.4E'
-ScalarBar.RangeLabelFormat = '%2.2E'
-					 
-GetRenderView().Representations.append(ScalarBar)
-ScalarBar.LookupTable = Vitesse_LUT
-print("\nFinished Processing VTU files'")
+	ScalarBar.LabelBold = 0
+	ScalarBar.LabelItalic = 0
+	ScalarBar.LabelOpacity = 0
+	ScalarBar.LabelShadow = 0
+	ScalarBar.LabelColor = RED
+	ScalarBar.LabelFontSize = 10
+	ScalarBar.LabelOpacity = 1.0
+	ScalarBar.LabelFontFamily = 'Arial'
+	ScalarBar.NumberOfLabels = 0
+	ScalarBar.DrawTickMarks = 0
+	ScalarBar.DrawTickLabels = 0
+	ScalarBar.AddRangeAnnotations = 1 # min and max on scalarbar
+	ScalarBar.DrawNanAnnotation = 0
+	ScalarBar.NanAnnotation = 'NaN'
+
+	ScalarBar.AutomaticLabelFormat = 0
+	ScalarBar.LabelFormat = '%-#2.2E'
+	ScalarBar.RangeLabelFormat = '%2.4e'
+						 
+	GetRenderView().Representations.append(ScalarBar)
+	ScalarBar.LookupTable = Vitesse_LUT
+	print("\nFinished Processing VTU files'")
+
 
 # LOOP OVER FILES AND EXPORT PNG
 # ================================
-if EXPORT_PNG is True:
-	print("\n\nEXPORTING ...")
-	if not os.path.exists(dirname + foldername + 'img'):
-		os.makedirs(dirname + foldername + 'img')
+img_dir = dirname + foldername + 'img\\'
+
+if EXPORT_PNG :
+	print("\n\nEXPORTING ...\n")
+	if not os.path.exists(img_dir):
+		os.makedirs(img_dir)
 	
 	for t in timesteps:
 		# if t > 4: break
 		view.ViewTime = t	
 		custom_view(view)
-		data_representation.RescaleTransferFunctionToDataRange(True)
+		# data_representation.RescaleTransferFunctionToDataRange()
 		Render() 
 		
-		filename = dirname + foldername + "img\\" + "Frame " + str(t)+ ".png"
-		# filename = "Frame " + str(t)+ ".png"
+		imgname = "Frame_" + str(t)[:-2].rjust(5,'0') + ".png"
+		filename = img_dir + imgname
+		# [:-2] strips the deicmal part coming from paraview's timestep
+		# rjust(5,'0') returns a new string with leading zeros 
+		# LEADING ZEROS ARE CRUCIAL FOR THE SORTING ALGORITHM USED DURING FILE LISTING (Dir) BEFORE ANIMATION
 		
 		# EXPORT PNG METHODS
 		# ==================
@@ -187,7 +262,7 @@ if EXPORT_PNG is True:
 		# aLayout = GetLayout()
 		""" METHOD 2: works only with Paraview 4.2 and above: allows more options and control """
 		SaveScreenshot(filename, layout=None, magnification=1, quality=1)
-		print("'Frame " + str(t)+ ".png' was successfully exported")
+		print(imgname + " was successfully exported")
 		
 		""" METHOD 3: http://www.paraview.org/ParaView3/Doc/Nightly/www/py-doc/paraview.simple.html#paraview.simple.WriteAnimation"""
 		# WriteAnimation(	filename, 
@@ -197,25 +272,33 @@ if EXPORT_PNG is True:
 						# Quality=2,
 						# BackgroundColor = (1.0,1.0,1.0)
 						# )
-print("\nFinished exporting PNG files from VTU files")
+	print("\nFinished exporting PNG files from VTU files")
 
 
-if CROP_PNG is True:
+if CROP_PNG :
 	print("\n\nCROPPING ...")
+	if not os.path.exists(img_dir):
+		raise Exception("\nThe directory '" + img_dir + "' does not exist !")
 	command = ('mogrify',
 			   "-format",
 			   "png",
 			   "-trim",
-			   dirname + foldername + 'img\\*.png'
+			   img_dir+'*.png'
 			   )
-	subprocess.check_call(command)		   
+	subprocess.call(command)		   
 	print("\nFinished cropping PNG files")
 
 	
-if MAKE_ANIMATION is True:	
+if MAKE_ANIMATION :	
 	print("\n\nCREATING ANIMATION ...")
+	if not os.path.exists(img_dir):
+		raise Exception("\nThe directory '" + img_dir + "' does not exist !")
+	os.chdir(img_dir)
+	subprocess.call('dir /b /o *.png > files.txt', shell=True)	
+	
+	animationname = "Animation_" + foldername[:-1].lower() + ".avi"
 	command = ('mencoder',
-			   'mf://'+ dirname + foldername + 'img\\*.png',
+			   'mf://@files.txt', #works only in the current dir
 			   '-mf',
 			   'type=png:fps='+str(fps),
 			   '-ovc',
@@ -225,10 +308,10 @@ if MAKE_ANIMATION is True:
 			   '-oac',
 			   'copy',
 			   '-o',
-			   'ANIMATION.avi')		   
-	subprocess.check_call(command)		   
-	print("\nANIMATION.avi was created from PNG files")
+			   dirname + animationname )   
+	subprocess.call(command)	
+	
+	os.chdir(dirname)	
+	print("\n\n\nANIMATION.avi was created from PNG files")
 		   
-		   
-		   
-		   
+	   
